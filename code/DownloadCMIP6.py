89340e4ebbc0a97de4f262b739a25a1e611193c2
Manuel del Jesus <manuel.deljesuspe@gmail.com>
2021-07-13 10:48:25 +0200

    Añadido script de descarga de datos
    He añadido el script DownloadCMIP6.py para descargar datos de CMIP6
    desde ESGF. Funciona bastante «mal» el sistema, porque no siempre
    detecta todos los ficheros, ni siempre descarga todos los modelos.
    Creo que por el momento nos sirve para tirar y hacer pruebas,
    pero habrá que revisar qué modelos faltan y descargar los ficheros
    específicos.
    
     On branch manu
     Changes to be committed:
    	modified:   .gitignore
    	modified:   code/DownloadCMIP6.py

--- code/DownloadCMIP6.py
+++ code/DownloadCMIP6.py
@@ -1,14 +1,22 @@
 from pyesgf.search import SearchConnection
 import xarray as xr
+import numpy as np
 import os.path as op
 from tqdm import tqdm
 from datetime import datetime
 
-conn = SearchConnection('http://esgf-data.dkrz.de/esg-search',
+Alemania = "https://esgf-data.dkrz.de/esg-search/"
+Francia = "https://esgf-node.ipsl.upmc.fr/esg-search/"
+
+conn = SearchConnection(Francia,
                         distrib=True)
 
+# We need to download variables:
+# prc: Convective precipitation
+# pr: Non-convective precipitation
+VAR_NAME = 'pr'
 ctx = conn.new_context(project='CMIP6',
-                       variable='prc',
+                       variable=VAR_NAME,
                        experiment_id="historical",
                        frequency="day")
 datos = ctx.search()
@@ -20,17 +28,22 @@ for idx, i in tqdm(enumerate(datos), total=len(datos)):
     fc = fc[0]
     if fc.opendap_url is None:
         continue
-    outfile = op.join("./CMIP6", op.basename(fc.opendap_url))
+    outfile = op.join("/srv/SystematicClimate/CMIP6", op.basename(fc.opendap_url))
     if op.isfile(outfile):
         continue
     try:
         with xr.open_dataset(fc.opendap_url) as ds:
-            prc = ds["prc"]
-            prc = prc.sel(lat=slice(43, 44), lon=slice(355, 357))
+            prc = ds[VAR_NAME]
+            prc = prc.sel(lat = slice(34, 46),
+                          lon = prc.lon[(prc.lon<5) | (prc.lon>350)])
+            prc = prc.assign_coords(lon = (((prc.lon + 180) % 360) - 180))
+            prc = prc.roll(lon = (-np.nonzero(prc.lon.values < 0)[0][0]), roll_coords=True)
             prc.to_netcdf(outfile)
-    except:
+    except Exception as e:
         print("Ha habido un problema con el fichero: ", op.basename(fc.opendap_url))
+        print(e)
         print("Seguimos a por el próximo")
         with open("./ErroresDescarga.txt", "at") as fid:
             fid.write(op.basename(fc.opendap_url))
             fid.write("\n")
+        
